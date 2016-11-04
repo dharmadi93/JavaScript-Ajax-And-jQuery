8a97a3414a7923d1cf83ffe8348b9656bd0de504
Dharmadi Tanamas <dharmadi93@gmail.com>
2016-11-04 17:19:56 +0700

    changin colors works

--- client/script.js
+++ client/script.js
@@ -5,6 +5,31 @@ function getColor() {
         contentType: 'application/x-www-form-urlencoded',
         success: function (data) {
             console.log(data)
+            selectCol(data)
         }
     })
+}
+
+function selectCol(data) {
+    var x = Math.floor((Math.random() * 9) + 1);
+    if($("div").find(`#col${x}`).hasClass("red")) {
+        $("div").find(`#col${x}`).removeClass("red")
+    }
+    else if($("div").find(`#col${x}`).hasClass("green")) {
+        $("div").find(`#col${x}`).removeClass("green")
+    }
+    else if($("div").find(`#col${x}`).hasClass("blue")) {
+        $("div").find(`#col${x}`).removeClass("blue")
+    }
+    else if($("div").find(`#col${x}`).hasClass("yellow")) {
+        $("div").find(`#col${x}`).removeClass("yellow")
+    }
+    else if($("div").find(`#col${x}`).hasClass("pink")) {
+        $("div").find(`#col${x}`).removeClass("pink")
+    }
+    else if($("div").find(`#col${x}`).hasClass("orange")) {
+        $("div").find(`#col${x}`).removeClass("orange")
+    }
+
+    $("div").find(`#col${x}`).addClass(data)
 }
